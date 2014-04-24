022215f4956444d29fef18b92ad811bfc9c02069
victormedel <victor.medel@roadrunner.com>
2014-04-24 00:55:23 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

--- MidTerm/MidTerm_Prb6/nbproject/Makefile-variables.mk
+++ MidTerm/MidTerm_Prb6/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=mitterm_prb6
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/mitterm_prb6
+CND_ARTIFACT_NAME_Debug=midterm_prb6
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/midterm_prb6
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=mittermprb6.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/mittermprb6.tar
+CND_PACKAGE_NAME_Debug=midtermprb6.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/midtermprb6.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=mitterm_prb6
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/mitterm_prb6
+CND_ARTIFACT_NAME_Release=midterm_prb6
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/midterm_prb6
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=mittermprb6.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/mittermprb6.tar
+CND_PACKAGE_NAME_Release=midtermprb6.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/midtermprb6.tar
 #
 # include compiler specific variables
 #
