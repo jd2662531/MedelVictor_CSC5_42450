b846ac12688811cd3ff806225d373359545ecb0e
victormedel <victor.medel@roadrunner.com>
2014-05-31 22:51:07 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

--- C++ Project 2/Black_Jack_Version2_rev2/main.cpp
+++ C++ Project 2/Black_Jack_Version2_rev2/main.cpp
@@ -6,6 +6,7 @@
  * 
  */
 
+//System Libraries
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -16,24 +17,33 @@ using namespace std;
 
 
 //Function Prototypes
-void crdSuit( );
+void dealCrd(string card[], int& randCard, string suit[], int& randSuit);
+
+
 
 //Execution Starts Here
 int main(int argc, char** argv) {
+    //Variable Declaration and Random Seed 
     srand(time(0));
+    int totl;
+    int randCard, randSuit;
+    string suit[4]={"Hearts","Diamonds","Spades","Clubs"};
+    string card[14]={"Ace","2","3","4","5","6","7","8","9","10","Ace","Jack","Queen","King"};    
+    //Function Call
     
+    dealCrd(card,randCard,suit,randSuit);
+    cout<<" | ";
+    dealCrd(card,randCard,suit,randSuit);
+    cout<<"\ntotal:";
     
-    
-    crdSuit( );
-    
-  
     
     //Exit Stage Right
     return 0;
 }
-void crdSuit( ){
-    
-    string suit[4]={"Hearts","Diamonds","Spades","Clubs"};
-    int randSuit=rand()%4;
+void dealCrd(string card[], int& randCard, string suit[], int& randSuit){
+    randSuit=rand()%4;
+    randCard=rand()%14;
+    cout<<card[randCard];
+    cout<<" ";
     cout<<suit[randSuit];
 }
---
+++ C++ Project 2/Black_Jack_Version2_rev3/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Victor Medel
+ * Created on May 31, 2014, 7:21 PM
+ * CSC 5 (42450) | Project 2: Black Jack Game Version 2
+ */
+
+//System Libraries
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+//Global Constants
+
+
+//Function Prototypes
+
+
+//Execution Starts Here
+int main(int argc, char** argv) {
+   
+    
+    //Exit Stage Right
+    return 0;
+}
+
+
