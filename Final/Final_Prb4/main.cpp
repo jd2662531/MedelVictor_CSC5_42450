66828ae3dd4b4b6b91949d8d94730f030e73df6a
victormedel <victor.medel@roadrunner.com>
2014-06-11 10:26:56 -0700

    Signed-off-by: victormedel <victor.medel@roadrunner.com>

--- Final/Final_Prb4/main.cpp
+++ Final/Final_Prb4/main.cpp
@@ -6,23 +6,40 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 //Global Constants
-const int n=5, ntimes=10000;
+
 
 //Function Prototypes
-
+int pickNum(int [], int);
 
 //Execution Starts Here
 int main(int argc, char** argv) {
     //Declare Variables
-    int freq[n]={0};
-    int rndseq[]={5,51,78,181,208};
-    
-    
+    srand(time(0));
+    const int N=5, NTIMES=10000;
+    int freq[]={0,0,0,0,0};
+    int rndSeq[]={9,51,78,181,208};
+    int rndm;
+//Output randomize the number NTIMES and output their frequency    
+ for(int i = 0; i<NTIMES; i++){
+    rndm = pickNum(rndSeq, N);
+        for(int j = 0; j<N ; j++){
+            if(rndm == rndSeq[j]){
+                freq[j]++;
+             }  
+         }
+}
+for(int i = 0; i<N; i++){
+cout<<rndSeq[i]<<" occurred "<<freq[i]<<" times."<<endl;
+}
     //Exit Stage Right
     return 0;
 }
-
-void
+int pickNum(int rndSeq[], int num){
+    num;
+    num = rand() % 5;
+    return rndSeq[num];
+}
